cd37f20bdff89a3ef337ab10978cadf58fb3783d
divhenr <henrique_graca@hotmail.com>
2015-11-15 23:32:48 +0000

    Services

---
+++ src/HelpDeskVNext.Services/SmsProvider/TwilioSmsProvider.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//  using Twilio;
+
+namespace HelpDeskVNext.Services.SmsProvider
+{
+    public class TwilioSmsProvider
+    {
+        public TwilioSmsProvider()
+        {
+//              var client = new TwilioRestClient("your-twilio-account-sid", "your-twilio-auth-token");
+//              
+//              // Select all calls from my account
+//              var calls = client.ListCalls(new CallListRequest());
+//   
+//              // Check for any exceptions
+//              if (calls.RestException != null)
+//              {
+//                  throw new FormatException(calls.RestException.Message);
+//              }
+//   
+//              // Loop through them and show information
+//              foreach(var call in calls.Calls){
+//                  var detail = String.Format("From: {0}, Day: {1}, Duration: {2}s", call.From, call.DateCreated, call.Duration);
+//                  //Console.WriteLine(detail);
+//              }
+        }
+    }
+}
